9195697297070e900b897d38aa8be636a82a9e01
arthurmiy <arthurmiy@hotmail.com>
2023-08-19 15:55:41 -0300

    url launcher error correction 2, more workers for parallel upload

--- htdocs/web/flutter_service_worker.js
+++ htdocs/web/flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "50c596edc3e1ecafaf7fe6d241067e10
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "794e05c2abbabe97a5160400f7762edc",
-"/": "794e05c2abbabe97a5160400f7762edc",
-"main.dart.js": "b0db4020098db421bca20aa574919f23",
+"index.html": "6cc5b285025980983dfd7142e9cc21e0",
+"/": "6cc5b285025980983dfd7142e9cc21e0",
+"main.dart.js": "32d3dd406f5f7bb3316baaee211e75fa",
 "manifest.json": "dc5376803cf5a7dd73da8d64e365e6f0",
 "version.json": "c631d64826a2a723f0dd3660ffc9ce41"};
 // The application shell files that are downloaded before a service worker can
